44a427fcb07f2dc1473708a688a7fd15200b8fbd
tummm <tlvnhantam47@gmail.com>
2022-05-16 20:45:43 +0700

    new_folder

---
+++ Day 41/main.py
@@ -0,0 +1,44 @@
+import random
+from replit import clear
+# from art import logo
+
+# print(logo)
+word_list = ["conga", "logan", "mango"]
+chosen_word = random.choice(word_list)
+
+# print(chosen_word)
+
+word_blank = []
+for blank in range(0, len(chosen_word)):
+     word_blank += "_"
+print(f"{word_blank} \n")
+
+end_game = False
+live_reduce = 3
+
+while not end_game:
+    player_guess = input("Let's guess a letter !!! ").lower()
+    clear()
+    if player_guess in chosen_word:
+        print(f"You already guess {player_guess}")
+
+    for i in range(len(chosen_word)):
+        letter = chosen_word[i]
+        if letter == player_guess:
+            word_blank[i] = letter
+
+    if player_guess not in chosen_word:
+        print(f"{player_guess} not in the word")
+        live_reduce -= 1
+        print(f"You have {live_reduce} life left !!! \n")
+    print(f"{word_blank} \n")
+\
+
+    if live_reduce == 0:
+        print("You lose")
+        break
+
+    if "_" not in word_blank:
+        end_game = True
+        print("You win")
+
